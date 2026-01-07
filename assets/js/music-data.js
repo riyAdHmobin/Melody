458e7dd6f94dd1e1df3fa2edad99283fbfe6d975
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2026-01-07 15:15:26 +0600

    PWA Integrated

--- assets/js/music-data.js
+++ assets/js/music-data.js
@@ -14,13 +14,4 @@ const musicData = [
         artist: "No Spirit x Tonion",
         musicPath: "./assets/music/afusic-pal-pal.mp3",
     },
-    {
-        backgroundImage: "./assets/images/poster-1.jpg",
-        posterUrl: "./assets/images/poster-1.jpg",
-        title: "Afusic Pal Pal",
-        album: "No Spirit",
-        year: 2022,
-        artist: "No Spirit x Tonion",
-        musicPath: "./assets/music/afusic-pal-pal.mp3",
-    },
 ];
